2b1c37e38fcb91e4cb2b7487515c7825be67acef
Thales <thaleswat2005@gmail.com>
2026-03-10 19:32:26 -0300

    alterações no site da professora

---
+++ TarefasCasa/1º Semana/script.js
@@ -0,0 +1,8 @@
+    const toggleButton = document.getElementById('toggleSidebar');
+    const sidebar = document.querySelector('.sidebar');
+    const content = document.querySelector('.content');
+
+    toggleButton.addEventListener('click', () => {
+	sidebar.classList.toggle('hidden');
+	content.classList.toggle('full');
+    });
