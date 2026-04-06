9e9281d8b1a50ffddf81baa84ce1459ab8b42c65
Thales <thaleswat2005@gmail.com>
2026-04-06 20:47:37 -0300

    Curtidas + Jupiter + PaginaProfessor, sem o Shift+AltGr+P porque no macos não aparenta funcionar, verei em algum linux depois

--- TarefasCEC/5º Semana/main.py
+++ TarefasCEC/5º Semana/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 contador = 0
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory=["Templates", "Templates/Partials"])
 
 @app.get("/",response_class=HTMLResponse)
 async def root(request: Request):
     
-    return templates.TemplateResponse(request, "index.html", {"contador": "0"})
+    return templates.TemplateResponse(request, "index.html")
 
 
 @app.post("/curtir", response_class=HTMLResponse)
@@ -22,3 +24,23 @@ async def curtir(request : Request):
         return templates.TemplateResponse(request, "index.html")
     return str(contador)
 
+@app.put("/zerar", response_class=HTMLResponse)
+async def zerar(request : Request):
+    global contador
+    contador=0
+    if (not "HX-Request" in request.headers):
+        return templates.TemplateResponse(request, "index.html")
+    return str(contador)
+
+@app.get("/curtidas", response_class=HTMLResponse)
+async def curtidas(request : Request):
+    global contador
+    return templates.TemplateResponse(request, "contador.html",  {"contador": contador})
+
+@app.get("/professor", response_class=HTMLResponse)
+async def prof(request : Request):
+    return templates.TemplateResponse(request, "paginaprofessor.html")
+
+@app.get("/jupiter", response_class=HTMLResponse)
+async def jup(request : Request):
+    return templates.TemplateResponse(request, "jupiter.html")
